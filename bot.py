f3242bfba3389806c9bfc301d17a6767306364f0
paulostationbr <34623594+paulostationbr@users.noreply.github.com>
2018-12-11 13:25:54 -0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,32 @@
+print('Iniciando o bot...')
+
+
+import amanobot
+from amanobot.loop import MessageLoop
+import time
+import aiml
+import config
+
+
+bot = amanobot.Bot(config.token)
+
+k = aiml.Kernel()
+k.learn('aiml/*.aiml')
+
+
+def handle(msg):
+	if msg.get('text'):
+		response = k.respond(msg['text'])
+		if response:
+			response = response.replace('$nome', msg['from']['first_name']).replace('#', '\n')
+			bot.sendChatAction(msg['chat']['id'], 'typing')
+			time.sleep(1)
+			bot.sendMessage(msg['chat']['id'], response)
+
+
+MessageLoop(bot, handle).run_as_thread()
+
+print('Bot iniciado!')
+
+while True:
+	time.sleep(10)
